068d6e126e522991152013db1bd41243f62d18e2
Andrew Rauh <andrew@andrewrauh.com>
2013-01-24 23:48:09 -0500

    first

---
+++ HAUS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HAUS
+//
+//  Created by Andrew Rauh on 1/24/13.
+//  Copyright (c) 2013 Andrew Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HAUS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HAUS
+//
+//  Created by Andrew Rauh on 1/24/13.
+//  Copyright (c) 2013 Andrew Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
